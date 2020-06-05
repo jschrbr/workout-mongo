9d391e64eb38eebc1afbb1cb1c1e0c986d25a58f
jschrbr <schrbrr@gmail.com>
2020-06-06 02:47:19 +1000

    oops

--- public/exercise.js
+++ public/exercise.js
@@ -112,9 +112,7 @@ async function handleFormSubmit(event) {
     workoutData.reps = Number(repsInput.value.trim());
     workoutData.duration = Number(resistanceDurationInput.value.trim());
   }
-  if (shouldNavigateAway) {
-    return (location.href = "/");
-  }
+
   await API.addExercise(workoutData);
   clearInputs();
   toast.classList.add("success");
@@ -122,6 +120,9 @@ async function handleFormSubmit(event) {
 
 function handleToastAnimationEnd() {
   toast.removeAttribute("class");
+  if (shouldNavigateAway) {
+    location.href = "/";
+  }
 }
 
 function clearInputs() {
